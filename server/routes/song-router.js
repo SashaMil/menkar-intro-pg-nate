eeb2c1a40b0de02ab0bffcb4dfaa3b231ac56713
Sasha Milenkovic <sashamilenkovic2@gmail.com>
2018-06-07 13:26:29 -0500

    done

--- server/routes/song-router.js
+++ server/routes/song-router.js
@@ -19,39 +19,47 @@ router.get('/', (req, res) => {
     })
 });
 
-router.post('/', (req, res) => {
-  console.log('In song-router POST to create');
-  // Post generally uses the request body to get the data to add/insert/create
-
-  res.sendStatus(200);
-});
-
-router.put('/', (req, res) => {
-  // Put generally uses request params to identify the thing to update
-
-  // And also uses the request body to get the data to update
-
-  console.log('In song-router PUT to update');
-  res.sendStatus(200);
-});
-
-router.delete('/:id', (req, res) => {
-  // Delete generally uses request params to identify the thing to delete
-  const songId = req.params.id;
-  console.log('In song-router DELETE to delete the song with id:', songId);
-
-  const queryText = 'DELETE FROM songs WHERE id=$1';
-  // Passing two things to the query. 1) the query text
-  // 2) an array of values to substitute into the query for the $1, $2, etc. 
-  //    when subbing in multiple things, the order is important.
-  pool.query(queryText, [songId])
-    .then((results)=>{
-      console.log('Successful delete of song', results);
+router.post('/', (req, res)=> {
+   console.log('Handling POST for /song', req.body);
+   const queryText = `INSERT INTO songs (artist, track, published, rank)
+                        Values
+                        ($1, $2, $3, $4);`;
+   pool.query(queryText, [req.body.artist, req.body.track, req.body.published, req.body.rank])
+   .then((result)=>{
+      console.log('Finished POST for /song', req.body);
       res.sendStatus(200);
-    }).catch((err)=>{
-      console.log('Error deleting of song', err);
+   })
+   .catch((error)=> {
+      console.log('Error handling POST for /song', error);
       res.sendStatus(500);
-    })
+   });
+});
+
+router.put('/:id', (req, res)=> {
+   console.log('Handling PUT for /song', req.body);
+   const id = req.params.id;
+   const queryText = 'UPDATE songs SET artist = $2, track = $3, published = $4, rank = $5 WHERE id = $1;';
+   console.log(queryText);
+   pool.query(queryText, [id, req.body.artist, req.body.track, req.body.published, req.body.rank]).then((result)=> {
+      console.log('Finished PUT for /song: ', result);
+      res.sendStatus(200);
+   }).catch((error)=>{
+      console.log('Error handling PUT for /song: ', error);
+      res.sendStatus(500);
+   });
+});
+
+router.delete('/:id', (req, res)=> {
+   console.log('Handling DELETE for /song');
+   const id = req.params.id;
+   const queryText = `DELETE FROM songs WHERE id = $1`;
+   pool.query(queryText, [id]).then((result)=> {
+      console.log('Deleted from /song', id);
+      res.sendStatus(200);
+   }).catch((error)=> {
+      console.log('Error handling DELETE for /song', error);
+      res.sendStatus(500);
+   });
 });
 
 module.exports = router;
